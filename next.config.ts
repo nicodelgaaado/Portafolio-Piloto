ddef3d3bf4edd2cc0f0ce887cfcee28bcccdbfbf
nicolas <nicolas.delgadop@campusuuc.edu.co>
2026-03-23 02:15:00 -0500

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dtiiihvipieempqflzmj.supabase.co",
+      },
+    ],
+  },
   turbopack: {
     root: process.cwd(),
   },
